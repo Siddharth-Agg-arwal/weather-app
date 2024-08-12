de106dd08f8c1da8a8d19dd968697a0da3904414
Siddharth-Agg-arwal <aggarwal.siddharth2003@outlook.com>
2024-08-12 22:55:46 +0530

    dynamic render with handlebars

---
+++ web-server/public/js/trial.js
@@ -0,0 +1,1 @@
+console.log('running client side JS')
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,9 +1,50 @@
+const path = require('path')
 const express = require('express')
+
+
+// console.log(__dirname)
+// console.log(path.join(__dirname, '../public'))
 
 const app = express()
 
+//define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates')
+
+
+//setup handlebars engine and view location
+app.set('views', viewsPath)
+app.set("view engine", "hbs");
+
+//setup static dir to serve
+app.use(express.static(publicDirectoryPath))
+
+
 app.get('', (req, res) => {
-    res.send('Hello express!')
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Sid',
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        helpText: 'lmao kys.'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast : '50 degrees',
+        location : 'Patiala'
+    })
 })
 
 app.listen(3000, () => {
