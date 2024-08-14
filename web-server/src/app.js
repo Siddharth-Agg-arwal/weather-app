fde104c14000247d7559f270632be8dc3a8ad768
Siddharth-Agg-arwal <aggarwal.siddharth2003@outlook.com>
2024-08-15 03:50:49 +0530

    little designing

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -50,6 +50,22 @@ app.get('/weather', (req, res) => {
     })
 })
 
+app.get('/help/*', (req, res) => {
+    res.render('404',{
+        title : '404',
+        name : 'Sid',
+        errorMsg : 'Help article not found.',
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404',{
+        title : '404',
+        name : 'Sid',
+        errorMsg : '404, page not found',
+    })
+})
+
 app.listen(3000, () => {
     console.log('Server is up on port 3000')
 })
