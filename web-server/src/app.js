0ee1f5dd9a404e2e421f9f7467d37e65ca7677e7
Siddharth-Agg-arwal <aggarwal.siddharth2003@outlook.com>
2024-08-16 02:09:33 +0530

    fetch

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const weather = require('./utils/weather')
 
 // console.log(__dirname)
 // console.log(path.join(__dirname, '../public'))
@@ -44,9 +46,50 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
+
+    if(!req.query.address){
+        return res.send({
+            error : 'Enter a valid address.'
+        })
+    }
+
+    geocode(req.query.address, (error, {latitude, longitude, location}= {}) => {
+        if(error){
+            return res.send({error})
+        }
+        
+        weather(latitude, longitude, (error, forecastData) => {
+            if(error){
+                return res.send({error})
+            }
+
+            res.send({
+                location : location, 
+                forecast: forecastData
+            })
+            // response.send(forecastData)
+        })
+    })
+
+    // res.send({
+    //     forecast : '50 degrees',
+    //     location : 'Patiala',
+    //     address : req.query.address
+    // })
+    
+})
+
+app.get('/products', (req, res) => {
+
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+
+    console.log(req.query)
     res.send({
-        forecast : '50 degrees',
-        location : 'Patiala'
+        products : [] 
     })
 })
 
---
+++ web-server/src/utils/geocode.js
@@ -0,0 +1,43 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/search/geocode/v6/forward?q=' + encodeURIComponent(address) + '&proximity=ip&access_token=pk.eyJ1Ijoic2lkZGhhcnRoLWFnZy1hcndhbCIsImEiOiJjbHpvaTVtenEwdmt4MmlxeTg0MngwdDBpIn0.iafy9pFekyijpxnC_TDUGA'
+
+    request({ url, json: true}, (error, {body}) => {
+        // if(error){
+        //     callback('Unable to connect to location services.', undefined) //adding undefined is optional as its the default value.
+        // }
+        if(body.features.length === 0){
+            callback('Try again for another location')
+        }
+        else{
+            callback(undefined, {
+                latitude : body.features[0].properties.coordinates.latitude,
+                longitude : body.features[0].properties.coordinates.longitude,
+                location : body.features[0].properties.name
+            })
+        }
+    })
+}
+
+module.exports = geocode
+
+
+
+// const geocodeURL = 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=434ab6f6e0441c0b366be943d9f8498c'
+// const geocodeURL = 'https://api.mapbox.com/search/geocode/v6/forward?q=punjab&proximity=ip&access_token=pk.eyJ1Ijoic2lkZGhhcnRoLWFnZy1hcndhbCIsImEiOiJjbHpvaTVtenEwdmt4MmlxeTg0MngwdDBpIn0.iafy9pFekyijpxnC_TDUGA'
+
+// request({ url: geocodeURL, json: true}, (error, response) => {
+//     if(error){
+//         console.log('Unable to connect to location services')
+//     }
+//     else if(response.body.features.length === 0){
+//         console.log('unable to find the location')
+//     }
+//     else{
+//         const latitude = response.body.features[0].properties.coordinates.latitude
+//         const longitude = response.body.features[0].properties.coordinates.longitude
+//         console.log(latitude, longitude)
+//         // console.log(response.body.features[0].properties.coordinates)
+//     }
+// })
